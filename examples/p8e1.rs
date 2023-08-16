2027b19a4bd5c1cd01b12ee4dacd0fb3e6aa7788
Francesco Urbani <francescourbanidue@gmail.com>
2023-08-16 22:23:00 +0300

    Run rust fmt tool

--- examples/p8e1.rs
+++ examples/p8e1.rs
@@ -1,18 +1,18 @@
-extern crate cast;
-extern crate posit;
-
-use std::u8;
-
 use cast::f64;
+use posit::Posit;
 use posit::P8E1;
+use typenum::{U1, U31};
 
 fn main() {
     for i in 0u16.. {
         if i > u8::MAX as u16 {
-            break
+            break;
         }
 
         let p = P8E1::new(i as u8);
         println!("{:3} - {}", i, f64(p));
     }
+
+    let p2 = Posit::<u32, U31, U1>::new(2423);
+    println!("{}", p2);
 }
